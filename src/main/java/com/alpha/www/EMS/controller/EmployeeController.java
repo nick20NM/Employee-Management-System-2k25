1671ddb0ed4db215b9bfa4d1593952686d8057ce
Nick <masihnishit@gmail.com>
2025-01-31 20:03:02 +0530

    delete employee rest api completed

--- src/main/java/com/alpha/www/EMS/controller/EmployeeController.java
+++ src/main/java/com/alpha/www/EMS/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,11 @@ public class EmployeeController {
 			@RequestBody EmployeeDto employeeDto){
 		EmployeeDto updatedEmployee = employeeService.updateEmployee(id, employeeDto);
 		return ResponseEntity.ok(updatedEmployee);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> deleteEmployee(@PathVariable Long id){
+		employeeService.deleteEmployee(id);
+		return ResponseEntity.ok("Employee successfully deleted with id: " + id);
 	}
 }
--- src/main/java/com/alpha/www/EMS/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/alpha/www/EMS/service/impl/EmployeeServiceImpl.java
@@ -59,4 +59,12 @@ public class EmployeeServiceImpl implements EmployeeService {
 		return EmployeeMapper.mapToEmployeeDto(updatedEmployee);
 	}
 
+	@Override
+	public void deleteEmployee(Long employeeId) {
+		Employee employee = employeeRepository.findById(employeeId)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not found with id: " + employeeId));
+		employeeRepository.deleteById(employeeId);
+		
+	}
+
 }
